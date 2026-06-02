044d11091977d0657a9057e8db5f2fa7184806c2
marco <mpmarcotmarcos@gmail.com>
2026-06-01 22:26:01 -0400

    atualizado

--- app/src/main/java/com/example/marketgame/view/DetalhesCompras.kt
+++ app/src/main/java/com/example/marketgame/view/DetalhesCompras.kt
@@ -10,30 +10,21 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.marketgame.R
 
-class ListaJogos : AppCompatActivity() {
+class DetalhesCompras : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_lista_jogos)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.listaJogos)) { v, insets ->
+        setContentView(R.layout.activity_detalhes_compras)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.detalhesCompras)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val conta = findViewById<TextView>(R.id.btnContaMenu);
-        val btnComprar1 = findViewById<Button>(R.id.btnComprar1);
-
-        btnComprar1.setOnClickListener {
-            val intent = Intent(this, DetalhesCompras::class.java);
-            startActivity(intent)
-        }
-
-        conta.setOnClickListener {
-            val intent = Intent(this, ContaActivity::class.java)
-            startActivity(intent);
-        }
+        val voltar = findViewById<Button>(R.id.btnVoltar);
+         voltar.setOnClickListener {
+             val intent = Intent(this, ListaJogos::class.java);
+             startActivity(intent)
+         }
     }
-
-
 }
